9d40f59287dc38202c159abb1bd3ddc9aed4e7b2
jérémy Réchet <jeremyr@hotmail.fr>
2018-02-12 10:00:09 +0100

    Hello

---
+++ src/main/java/fr/poe/ci/rest/Hello.java
@@ -0,0 +1,14 @@
+package fr.poe.ci.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("hi/{name}")
+    public String hi(@PathVariable("name") String name) {
+        return "hi " + name;
+    }
+}
